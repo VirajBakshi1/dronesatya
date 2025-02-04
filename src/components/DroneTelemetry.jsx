a8fafd97569dcb01116a003d80f96e8b0447cc43
Devil <64006580+SatyaNaiduQuickverse@users.noreply.github.com>
2025-02-04 09:38:29 +0530

    Update DroneTelemetry.jsx

--- src/components/DroneTelemetry.jsx
+++ src/components/DroneTelemetry.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
 // Replace with your Jetson's IP address
-const SOCKET_URL = 'http://172.29.172.210:5001';
+const API_URL = 'http://172.29.172.210:5001'
+const SOCKET_URL = 'ws://172.29.172.210:5001'
 
 const DroneTelemetry = () => {
   const [telemetryData, setTelemetryData] = useState({
